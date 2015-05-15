2a9fd7af3ffd8069f8a5f55e959c5a1645b35b4c
Djonnni <zavsup222@mail.ru>
2015-05-16 03:45:23 +0600

    6

---
+++ Purple/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Passport_data(models.Model):
+    surname = models.CharField(max_length=15)
+    name = models.CharField(max_length=15)
+    pytronymic = models.CharField(max_length=15)
+
+
+class Plane(models.Model):
+   # airline2 = models.ImageField()
+    airline = models.CharField(max_length=20)
+    privileges = models.BooleanField()
+    type_of_plane = models.CharField(max_length=20)
+    place_count = models.IntegerField()
+
+
+class Trip(models.Model):
+    point_of_departure = models.CharField(max_length=20)
+    point_of_destination = models.CharField(max_length=20)
+    data_of_departure = models.DateField()
+    time_of_departure = models.TimeField()
+    data_of_destination = models.DateField()
+    time_of_destination = models.TimeField()
+    plane_number = models.ForeignKey(Plane)
+    cost = models.IntegerField()
+
+
+class Tickets(models.Model):
+    trip_number = models.ForeignKey(Trip, primary_key=True)
+    passenger_id = models.ForeignKey(Passport_data, primary_key=True)
+    ticket_id = models.IntegerField(unique=True)
+    purchase_data = models.DateField()
+    purchase_time = models.TimeField()
+    purchase_cost = models.IntegerField()
+
+    #class Meta:
+     #   unique_together = ('trip_number', 'passenger_id')
+
+
+
+
+
